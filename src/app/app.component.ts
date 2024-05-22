aaead57c4972ccb885a26561ba844cb0630ff47e
C1334546 Carlos Gabriel Pantoja Alves <61744422+studymachinex@users.noreply.github.com>
2024-05-22 12:38:11 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { PickerComponent, PickerModule } from '@ctrl/ngx-emoji-mart';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PickerComponent, CommonModule, PickerModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'emojis-angular';
+  text: string = '';
+  showEmojiPicker: boolean = false;
+
+  toggleEmojiPicker() {
+    this.showEmojiPicker = !this.showEmojiPicker;
+  }
+
+  addEmoji(event: any) {
+    const emoji = event.emoji.native;
+    this.text += emoji;
+  }
+
+
 }
